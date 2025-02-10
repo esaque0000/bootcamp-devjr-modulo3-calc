a13e45ac56c0bf3aebcd859f033e2c580cfef984
Esaque Silva <esaquedasilva@gmail.com>
2025-02-10 15:44:01 -0300

    closes #5 - Implementar a lógica inicial do histórico

--- javascript.js
+++ javascript.js
@@ -12,4 +12,8 @@ function calc(op){
     }
 
     document.getElementById("output").value = output;
+
+    var newHistory = "<div>" + num1 + " " + op + " " + num2 + " = " + " " + output + "<div>";
+
+    document.getElementById("history").innerHTML = newHistory + document.getElementById("history").innerHTML;
 }
